fe23b19c0074600742a6d24d85d72054b3ecbef0
Léana KRAUTH <l.krauth@workstation17.nsi.lan>
2022-01-04 16:23:42 +0100

    creation de detup.py

--- creation_table.py
+++ creation_table.py
@@ -6,16 +6,63 @@ app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chevaux.sqlite3'
 
 db = SQLAlchemy(app)
 
+
+
 class cheval(db.Model):
-    Id = db.Column('cheval_id', db.Integer, primary_key = True)
+    Id = db.Column('ChevalId', db.Integer, primary_key = True)
     Nom = db.Column(db.String(50))
     Race = db.Column(db.String(50))
-    Dicipline = db.Column(db.String(20))
-#    PaysId = db.Column(db.Integere)   cle etrangere
+    PaysId = db.Column(db.Integer)
 
-    def __init__(self, name, city, addr,pin):
+    def __init__(self, Id, Nom, Race):
        self.Id = Id
-       self.Nom = nom
+       self.Nom = Nom
        self.Race = Race
+
+
+
+class pays(db.Model):
+    Id = db.Column('PaysId', db.Integer, primary_key = True)
+    Nom = db.Column(db.String(50))
+    Palmares = db.Column(db.String(50))
+
+    def __init__(self, Id, Nom, Palmares):
+       self.Id = Id
+       self.Nom = Nom
+       self.Palmares = Palmares
+
+
+
+class Cavalier(db.Model):
+    Id = db.Column('CavalierId', db.Integer, primary_key = True)
+    Nom = db.Column(db.String(50))
+    Prenom = db.Column(db.String(50))
+    PaysId = db.Column(db.Integer)
+
+    def __init__(self, Id, Nom, Prenom, Pays):
+       self.Id = Id
+       self.Nom = Nom
+       self.Prenom = Prenom
+       self.PaysId = PaysId
+
+
+
+class couple(db.Model):
+    Id = db.Column('CoupleId', db.Integer, primary_key = True)
+    CavalierId = db.Column(db.Integer)
+    ChevalId = db.Column(db.Integer)
+    Palmares = db.Column(db.String(50))
+    Dicipline = db.Column(db.String(20))
+
+    def __init__(self, Id, Cavalier, Cheval,Dicipline):
+       self.Id = Id
+       self.Cavalier = Cavalier
+       self.Cheval = Cheval
+       self.Palmares = Palmares
        self.Dicipline = Dicipline
+
 db.create_all()
+
+@app.route('/')
+def show_all():
+   return render_template('show_all.html', chevaux = cheval.query.all() )
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='projet',
+    version='1.0.0',
+    author='Leana',
+    author_email='krauth-l@saint-louis29.net',
+    description='Description',
+    packages=find_packages(),    
+    install_requires=['Flask', 'Plask-WTF'],
+)
